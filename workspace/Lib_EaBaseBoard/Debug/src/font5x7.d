a05cad022c3ce740c0beb3e6f4dfbdd8be43613b
Felipe Santos <felipecassiors@gmail.com>
2019-09-17 22:29:44 -0300

    Adicionado .gitignore

--- workspace/Lib_EaBaseBoard/Debug/src/font5x7.d
+++
@@ -1,4 +0,0 @@
-src/font5x7.o src/font5x7.d: ../src/font5x7.c \
- C:\Users\Evandro\Documents\LPCXpresso_8.2.2_650\BaseBoard\Lib_EaBaseBoard\inc/font_macro.h
-
-C:\Users\Evandro\Documents\LPCXpresso_8.2.2_650\BaseBoard\Lib_EaBaseBoard\inc/font_macro.h:
